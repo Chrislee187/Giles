c7894b313892ba63651088079af1ce458a2be75d
Chris Lee <chrislee187@gmail.com>
2023-03-14 10:33:33 +0000

    #codelove

--- Giles.Console/Program.cs
+++ Giles.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO.Enumeration;
+﻿using System.Diagnostics;
+using System.IO.Enumeration;
 using System.Reflection;
 
 namespace Giles.Console
@@ -11,42 +12,71 @@ namespace Giles.Console
 
             if (args.Length == 0)
             {
-                ShowError("No path specified");
-                ShowUsage();
-                Environment.Exit(1);
+                ShowOptionsError("No path specified", (int)OptionsErrors.PathNotSpecified);
             }
 
             if (!Directory.Exists(args[0]))
             {
-                ShowError($"Path not found: {args[0]}");
-                ShowUsage();
-                Environment.Exit(2);
+                ShowOptionsError($"Path not found: {args[0]}", (int)OptionsErrors.PathNotFound);
             }
 
             var path = args[0];
-            var w = new FileSystemWatcher(path);
-            w.IncludeSubdirectories = true;
 
-            ConsoleWatcher.Setup(w);
-            // ConsoleCountsWatcher.Setup(w);
+            var w = new FileSystemWatcher(path)
+            {
+                IncludeSubdirectories = true
+            };
+
+            var watchMode = GetWatchModeOption(args);
+
+            SetupWatcher(watchMode, w);
 
             w.EnableRaisingEvents = true;
-            
+
             System.Console.WriteLine($"Press any key to stop watching {Path.GetFullPath(path)}");
             System.Console.ReadKey();
         }
 
-
-
-        private static void ShowError(string msg)
+        private static void SetupWatcher(WatchMode watchMode, FileSystemWatcher w)
         {
-            System.Console.WriteLine(msg);
+            switch (watchMode)
+            {
+                case WatchMode.Default:
+                    ConsoleWatcher.Setup(w);
+                    break;
+                case WatchMode.Counts:
+                    ConsoleCountsWatcher.Setup(w);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
-        private static void ShowUsage()
+        private static WatchMode GetWatchModeOption(string[] args)
         {
-            System.Console.WriteLine("GILES path");
+            var watchMode = WatchMode.Default;
 
+            if (args.Length < 2) return watchMode;
+
+            switch (args[1].ToLower())
+            {
+                case "count":
+                case "counts":
+                    watchMode = WatchMode.Counts;
+                    break;
+                default:
+                    ShowOptionsError($"Invalid mode: {args[1]}", (int)OptionsErrors.InvalidWatchMode);
+                    break;
+            }
+
+            return watchMode;
+        }
+        
+        private static void ShowOptionsError(string msg, int exitCode = -1)
+        {
+            System.Console.WriteLine(msg);
+            ShowUsage();
+            Environment.Exit(exitCode);
         }
 
         private static void ShowHeader()
@@ -55,5 +85,20 @@ namespace Giles.Console
             var d = new FileInfo(asm.Location);
             System.Console.WriteLine($"Giles v{asm.GetName().Version} of {d.CreationTime}");
         }
+
+        private static void ShowUsage() => System.Console.WriteLine("GILES path-to-watch [count[s]]");
+    }
+
+    public enum OptionsErrors
+    {
+        PathNotSpecified = -1,
+        PathNotFound = -2,
+        InvalidWatchMode = -3
+
+    }
+
+    public enum WatchMode
+    {
+        Default, Counts
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Giles
+
+Simple folder watcher to see what files/folders are created, updated, deleted and renamed
+
+For example
+
+GILES C:
